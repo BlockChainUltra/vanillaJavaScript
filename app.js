abf15f30ff7f7b0d52b2fcc0f4fa7a8f34bb2d52
Malcolm <entid@protonmail.com>
2020-06-27 18:14:21 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+val = num1 + num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+console.log(val);
+
+val = Math.PI;
+val = Math.round(1.237);
+val = Math.ceil(1.237);
+val = Math.floor(1.237);
+val = Math.sqrt(4);
+val = Math.abs(-54);
+val = Math.pow(4, 4);
+val = Math.random() * 4;
+
+console.log(val);
+
