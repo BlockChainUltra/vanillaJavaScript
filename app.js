4c371b889490c40c3054ce974024b1f5e607ed00
Malcolm <entid@protonmail.com>
2020-06-28 12:02:05 +0900

    "Update"

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-const num1 = 100;
-const num2 = 50;
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+
+const numbers2 = new Array(234,23,23,23,4343);
+
+const fruit = ['apple', 'bannana', 'orange', 'pear'];
+
+const mixed = [33, 'death', true, 44, {a:1}];
+
 let val;
 
-val = num1 + num2;
-val = num1 * num2;
-val = num1 / num2;
-val = num1 % num2;
+// val = numbers.length;
+// val =Array.isArray(numbers);
+// val = numbers[3];
+// val = numbers.push(666,);
 
+// val = numbers.indexOf(36);
+
+// numbers.push(250);
+// numbers.unshift(0001);
+// val =numbers.pop();
+// val = numbers.splice(3, 3);
+// val = numbers.concat(numbers2);
+// val = fruit.sort();
+// val = numbers.sort();
+// val = numbers.sort(function(x, y){
+//     return x - y;
+// });
+
+function over50(num){
+    return num > 50;
+};
+
+val = numbers.find(over50);
+
+console.log(numbers);
 console.log(val);
-
-val = Math.PI;
-val = Math.round(1.237);
-val = Math.ceil(1.237);
-val = Math.floor(1.237);
-val = Math.sqrt(4);
-val = Math.abs(-54);
-val = Math.pow(4, 4);
-val = Math.random() * 4;
-
-console.log(val);
-
